0a1d015d12a9e213feda003de2cd60a45bad493b
thsgustlr0318 <thsgustlr0318@gmail.com>
2021-05-18 21:06:15 +0900

    for cors error

--- happyhouse_spring/src/main/java/com/ssafy/happyhouse/controller/QnAController.java
+++ happyhouse_spring/src/main/java/com/ssafy/happyhouse/controller/QnAController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -30,6 +31,7 @@ import com.ssafy.happyhouse.model.service.QnAService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
 @RequestMapping("/qna")
 @Api(value="HappyHouse QnA API")
