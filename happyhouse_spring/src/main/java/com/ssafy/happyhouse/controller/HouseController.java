a657988383e20b1818184b62b32db806dd084720
soyeon <ssafy.soyeon@gmail.com>
2021-05-19 15:43:21 +0900

    mapController modify

--- happyhouse_spring/src/main/java/com/ssafy/happyhouse/controller/HouseController.java
+++ happyhouse_spring/src/main/java/com/ssafy/happyhouse/controller/HouseController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,12 @@ import com.ssafy.happyhouse.model.dto.HouseInfoDto;
 import com.ssafy.happyhouse.model.dto.SidoGugunCodeDto;
 import com.ssafy.happyhouse.model.service.HouseService;
 
+import io.swagger.annotations.Api;
+
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
 @RequestMapping("/map")
+@Api(value="HappyHouse MAP API")
 public class HouseController {
 	private static final Logger logger = LoggerFactory.getLogger(HouseController.class);
 
