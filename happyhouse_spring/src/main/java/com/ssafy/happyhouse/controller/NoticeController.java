eab9444184ee4bf2b2151afd4835876b65d42098
thsgustlr0318 <thsgustlr0318@gmail.com>
2021-05-21 21:49:32 +0900

    공지 controller, mapper 설정

--- happyhouse_spring/src/main/java/com/ssafy/happyhouse/controller/NoticeController.java
+++ happyhouse_spring/src/main/java/com/ssafy/happyhouse/controller/NoticeController.java
@@ -7,14 +7,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.happyhouse.model.dto.Notice;
+import com.ssafy.happyhouse.model.dto.Question;
 import com.ssafy.happyhouse.model.service.NoticeService;
+
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
@@ -46,6 +52,29 @@ public class NoticeController {
 			// userid add
 			// permission error
 			noticeService.insertNotice(notice);
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@PutMapping(value = "/update")
+	public ResponseEntity<String> modify(@RequestBody Notice notice) {
+		try {
+			noticeService.modifyNotice(notice);
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+		}
+	}
+	
+	@DeleteMapping(value = "/delete/{no}")
+	public ResponseEntity<String> delete(@PathVariable String no) {
+		try {
+			System.out.println(no);
+			noticeService.deleteNotice(no);
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		} catch (Exception e) {
 			e.printStackTrace();
