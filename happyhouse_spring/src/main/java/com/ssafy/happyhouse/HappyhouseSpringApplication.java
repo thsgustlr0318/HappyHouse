93854428d0995869558e56f306838e449f9b0c34
thsgustlr0318 <thsgustlr0318@gmail.com>
2021-05-24 17:46:37 +0900

    공지 이미지 업로드

--- happyhouse_spring/src/main/java/com/ssafy/happyhouse/HappyhouseSpringApplication.java
+++ happyhouse_spring/src/main/java/com/ssafy/happyhouse/HappyhouseSpringApplication.java
@@ -2,10 +2,16 @@ package com.ssafy.happyhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.ssafy.happyhouse.property.FileUploadProperties;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableConfigurationProperties({
+	FileUploadProperties.class
+})
 public class HappyhouseSpringApplication {
 
 	public static void main(String[] args) {
